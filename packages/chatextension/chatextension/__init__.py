29038f95de67c0228952fb5509232dc176e73424
Fran Aguilera <fraguile@microsoft.com>
2024-01-08 01:03:51 -0500

    Added ChatExtension Jupyter endpoint.

---
+++ packages/chatextension/chatextension/__init__.py
@@ -0,0 +1,9 @@
+from .app import ChatExtensionApp
+
+
+def _jupyter_server_extension_points():
+    """
+    Returns a list of dictionaries with metadata describing
+    where to find the `_load_jupyter_server_extension` function.
+    """
+    return [{"module": "chatextension", "app": ChatExtensionApp}]
