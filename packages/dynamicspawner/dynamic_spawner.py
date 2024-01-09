3b3ec46333f85823e1a0c48668f4b572302ed049
Fran Aguilera <fraguile@microsoft.com>
2024-01-08 21:29:30 -0500

    Improve dynamic pvc and pv generation.

--- packages/dynamicspawner/dynamic_spawner.py
+++ packages/dynamicspawner/dynamic_spawner.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python
+import sys
+import copy
 from functools import partial
-from traitlets import List, Unicode
+from traitlets import Dict, List
+import asyncio
 from kubernetes_asyncio.client.models import (
     V1Pod,
+    V1ObjectMeta,
+    V1PersistentVolumeClaim,
+    V1PersistentVolumeClaimSpec,
+    V1ResourceRequirements,
+    V1PersistentVolume,
+    V1PersistentVolumeSpec,
     V1CSIPersistentVolumeSource,
     V1SecretReference,
 )
+from kubernetes_asyncio.client.rest import ApiException
 from jupyterhub.utils import exponential_backoff
 from kubespawner import KubeSpawner
-from kubespawner.objects import make_pvc
 
 
 class DynamicSpawner(KubeSpawner):
@@ -31,17 +40,17 @@ class DynamicSpawner(KubeSpawner):
         _add_init_container(volume_name, mount_path, sub_path): Add an initContainer for NFS mounts.
     """
 
-    sc_names = List(
-        trait=Unicode(),
+    additional_pvs = List(
+        trait=Dict(),
         default_value=[],
         config=True,
-        help="List of Storage Class names.",
+        help="Additional PVs to create dynamically.",
     )
-    pvc_templates = List(
-        trait=Unicode(), default_value=[], config=True, help="List of PVC templates."
-    )
-    volume_names = List(
-        trait=Unicode(), default_value=[], config=True, help="List of volume names."
+    additional_pvcs = List(
+        trait=Dict(),
+        default_value=[],
+        config=True,
+        help="Additional PVCs to create dynamically.",
     )
 
     def __init__(self, *args, **kwargs) -> None:
@@ -68,61 +77,130 @@ class DynamicSpawner(KubeSpawner):
 
         return template
 
+    async def _make_create_pv_request(self, pv, request_timeout):
+        # Try and create the pv. If it succeeds we are good. If
+        # returns a 409 indicating it already exists we are good. If
+        # it returns a 403, indicating potential quota issue we need
+        # to see if pv already exists before we decide to raise the
+        # error for quota being exceeded. This is because quota is
+        # checked before determining if the PV needed to be
+        # created.
+        pv_name = pv.metadata.name
+        try:
+            self.log.info(
+                f"Attempting to create pv {pv_name}, with timeout {request_timeout}"
+            )
+            await asyncio.wait_for(
+                self.api.create_persistent_volume(
+                    body=pv,
+                ),
+                request_timeout,
+            )
+            return True
+        except asyncio.TimeoutError:
+            # Just try again
+            return False
+        except ApiException as e:
+            if e.status == 409:
+                self.log.info(
+                    "PV " + pv_name + " already exists, so did not create new pv."
+                )
+                return True
+            elif e.status == 403:
+                t, v, tb = sys.exc_info()
+
+                try:
+                    await self.api.read_persistent_volume(pv_name)
+                except ApiException:
+                    raise v.with_traceback(tb)
+
+                self.log.info(
+                    "PV "
+                    + self.pv_name
+                    + " already exists, possibly have reached quota though."
+                )
+                return True
+            else:
+                raise
+
 
 async def modify_pod_hook(spawner: DynamicSpawner, pod: V1Pod):
-    if (
-        hasattr(spawner, "sc_names")
-        and hasattr(spawner, "pvc_templates")
-        and hasattr(spawner, "volume_names")
-        and len(spawner.sc_names)
-        == len(spawner.pvc_templates)
-        == len(spawner.volume_names)
-    ):
-        for idx in range(len(spawner.pvc_templates)):
-            sc_name = spawner.sc_names[idx]
-            pvc_template = spawner.pvc_templates[idx]
-            volume_name = spawner.volume_names[idx]
+    try:
+        spawner.log.info("Attempting to configure additional PVs and PVCs.")
+        if hasattr(spawner, "additional_pvs"):
+            for pv in spawner.additional_pvs:
+                await make_additional_pv(spawner, pv)
 
-            pvc_name = pvc_template.format(username=spawner.user.name)
+        if hasattr(spawner, "additional_pvcs"):
+            for pvc in spawner.additional_pvcs:
+                await make_additional_pvc(spawner, pvc)
+    except Exception as e:
+        spawner.log.error(f"Failed to configure additional PVs and PVcs: {e}")
+    finally:
+        return pod
 
-            await configure_additional_nfs_volume(
-                spawner, pod, sc_name, pvc_name, volume_name
+
+async def make_additional_pvc(
+    spawner: DynamicSpawner,
+    additional_pvc,
+):
+    """
+    Dynamically create an additional PVC based on provided configuration.
+    """
+    # Deep copy the configuration to avoid mutating the original object
+    pvc_config = copy.deepcopy(additional_pvc)
+
+    # Expand and set default values for metadata and spec
+    metadata = pvc_config.get("metadata", None)
+    if metadata:
+        pvc_name = metadata.get("name", None)
+        if pvc_name:
+            pvc_name = metadata["name"] = spawner._expand_user_properties(pvc_name)
+
+        metadata["labels"] = {
+            **spawner._build_common_labels(
+                spawner._expand_all(spawner.storage_extra_labels)
+            ),
+            **metadata.get("labels", {}),
+        }
+
+        metadata["annotations"] = {
+            **spawner._build_common_annotations(
+                spawner._expand_all(spawner.storage_extra_annotations)
+            ),
+            **metadata.get("annotations", {}),
+        }
+
+        pvc_config["metadata"] = metadata
+
+    spec = pvc_config.get("spec", None)
+    if spec:
+        volume_name = spec.get("volume_name", None)
+        if volume_name:
+            volume_name = spec["volume_name"] = spawner._expand_user_properties(
+                volume_name
             )
 
-    return pod
+        storage_class_name = spec.get("storage_class_name", None)
+        if storage_class_name:
+            spec["storage_class_name"] = spawner._expand_user_properties(
+                storage_class_name
+            )
 
+        selector = spec.get("selector", None)
+        if selector:
+            spec["selector"] = {
+                **spawner._expand_all(spawner.storage_selector),
+                **selector,
+            }
 
-async def configure_additional_nfs_volume(
-    spawner: DynamicSpawner,
-    pod: V1Pod,
-    sc_name: str,
-    pvc_name: str,
-    volume_name: str,
-):
-    pvc = make_pvc(
-        name=pvc_name,
-        storage_class=sc_name,
-        access_modes=spawner.storage_access_modes,
-        selector=spawner._expand_all(spawner.storage_selector),
-        storage=spawner.storage_capacity,
-        labels=spawner._build_common_labels(
-            spawner._expand_all(spawner.storage_extra_labels)
-        ),
-        annotations=spawner._build_common_annotations(
-            spawner._expand_all(spawner.storage_extra_annotations)
-        ),
-    )
+        pvc_config["spec"] = spec
 
-    pvc.spec.csi = V1CSIPersistentVolumeSource(
-        driver="blob.csi.azure.com",
-        volume_handle=f"stgptresearch002_{spawner.user.name}",
-        node_stage_secret_ref=V1SecretReference(
-            name="azure-storage-secret", namespace="aks-jupyterhub-dev-eastus-001"
-        ),
-    )
+    # Create and configure the PVC
+    pvc = make_pvc(**pvc_config)
 
-    spawner.log.debug(f"Attempting to create PVC {pvc_name}")
     try:
+        spawner.log.info(f"Creating PVC: {pvc}")
         await exponential_backoff(
             partial(
                 spawner._make_create_pvc_request,
@@ -135,3 +213,144 @@ async def configure_additional_nfs_volume(
         spawner.log.info(f"Successfully created PVC {pvc_name}")
     except Exception as e:
         spawner.log.error(f"Failed to create PVC {pvc_name}: {e}")
+
+
+async def make_additional_pv(
+    spawner: DynamicSpawner,
+    additional_pv,
+):
+    """
+    Dynamically create an additional PV based on provided configuration.
+    """
+    # Deep copy the configuration to avoid mutating the original object
+    pv_config = copy.deepcopy(additional_pv)
+
+    metadata = pv_config.get("metadata", None)
+    if metadata:
+        pv_name = metadata.get("name", None)
+        if pv_name:
+            pv_name = metadata["name"] = spawner._expand_user_properties(pv_name)
+
+        metadata["labels"] = {
+            **spawner._build_common_labels(
+                spawner._expand_all(spawner.storage_extra_labels)
+            ),
+            **metadata.get("labels", {}),
+        }
+
+        metadata["annotations"] = {
+            **spawner._build_common_annotations(
+                spawner._expand_all(spawner.storage_extra_annotations)
+            ),
+            **metadata.get("annotations", {}),
+        }
+
+        pv_config["metadata"] = metadata
+
+    spec = pv_config.get("spec", None)
+    if spec:
+        storage_class_name = spec.get("storage_class_name", None)
+        if storage_class_name:
+            spec["storage_class_name"] = spawner._expand_user_properties(
+                storage_class_name
+            )
+
+        csi = spec.get("csi", None)
+        if csi:
+            volume_handle = csi.get("volume_handle", None)
+            if volume_handle:
+                csi["volume_handle"] = spawner._expand_user_properties(volume_handle)
+
+            volume_attributes = csi.get("volume_attributes", None)
+            if volume_attributes:
+                container_name = volume_attributes.get("containerName", None)
+
+                if container_name:
+                    volume_attributes[
+                        "containerName"
+                    ] = spawner._expand_user_properties(container_name)
+
+                csi["volume_attributes"] = volume_attributes
+
+            spec["csi"] = csi
+
+        pv_config["spec"] = spec
+
+    # Create and configure the PV
+    pv = make_pv(**pv_config)
+
+    try:
+        spawner.log.info(f"Creating PV with config: {pv_config}")
+        spawner.log.info(f"PV class: {pv}")
+        await exponential_backoff(
+            partial(
+                spawner._make_create_pv_request,
+                pv,
+                spawner.k8s_api_request_timeout,
+            ),
+            f"Could not create PV {pv_name}",
+            timeout=spawner.k8s_api_request_retry_timeout,
+        )
+        spawner.log.info(f"Successfully created PV {pv_name}")
+    except Exception as e:
+        spawner.log.error(f"Failed to create PV {pv_name}: {e}")
+
+
+def make_pvc(
+    metadata=None,
+    spec=None,
+):
+    """
+    Make a k8s pvc specification for running a user notebook.
+    """
+    pvc = V1PersistentVolumeClaim()
+    pvc.kind = "PersistentVolumeClaim"
+    pvc.api_version = "v1"
+
+    if metadata:
+        pvc.metadata = V1ObjectMeta(**metadata)
+
+    if spec:
+        pvc.spec = V1PersistentVolumeClaimSpec(**spec)
+
+        resources = spec.get("resources", None)
+        if resources:
+            pvc.spec.resources = V1ResourceRequirements(**resources)
+
+        storage_class_name = spec.get("storage_class_name", None)
+        if storage_class_name:
+            pvc.metadata.annotations.update(
+                {"volume.beta.kubernetes.io/storage-class": storage_class_name}
+            )
+
+    return pvc
+
+
+def make_pv(
+    metadata=None,
+    spec=None,
+):
+    """
+    Make a k8s pv specification for running a user notebook.
+    """
+    pv = V1PersistentVolume()
+    pv.api_version = "v1"
+    pv.kind = "PersistentVolume"
+
+    if metadata:
+        pv.metadata = V1ObjectMeta(**metadata)
+
+    if spec:
+        pv.spec = V1PersistentVolumeSpec(**spec)
+
+        csi = spec.get("csi", None)
+        if csi:
+            pv.spec.csi = V1CSIPersistentVolumeSource(**csi)
+
+            node_stage_secret_ref = csi.get("node_stage_secret_ref", None)
+            if node_stage_secret_ref:
+                pv.spec.csi.node_stage_secret_ref = V1SecretReference(
+                    **node_stage_secret_ref
+                )
+
+    return pv
