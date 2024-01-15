8266863d2290c2c730337a640c3a02815dd97aee
Fran Aguilera <fraguile@microsoft.com>
2024-01-15 13:58:07 -0500

    Focus

--- packages/chatextension/chatextension/__init__.py
+++
@@ -1,9 +0,0 @@
-from .app import ChatExtensionApp
-
-
-def _jupyter_server_extension_points():
-    """
-    Returns a list of dictionaries with metadata describing
-    where to find the `_load_jupyter_server_extension` function.
-    """
-    return [{"module": "chatextension", "app": ChatExtensionApp}]
